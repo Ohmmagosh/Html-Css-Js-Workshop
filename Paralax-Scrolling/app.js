c4dfdb08760c6020f8eb0c53301817497ba3c637
OHMphatcha <phatcharaphol@hotmail.com>
2022-01-11 22:21:09 +0700

    change font

--- Paralax-Scrolling/app.js
+++ Paralax-Scrolling/app.js
@@ -5,7 +5,7 @@
         const wishElem = document.querySelector('.wish');
         
         moonElem.style.transform = `translate(${window.scrollY * 0.7}%, ${window.scrollY * -0.7}%)`;
-        wishElem.style.transform = `translateY(${window.scrollY * -1.2}%)`;
+        wishElem.style.transform = `translateY(${window.scrollY * -0.7}%)`;
     }
 
     function run(){
